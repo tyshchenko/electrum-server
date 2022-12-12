75544883fa86f9e1ceecaea7d4cd73d2245f2eec
root <root@validate.ankerid.com>
2022-12-12 14:44:22 +0000

    first commit

---
+++ src/networks.py
@@ -0,0 +1,14 @@
+# Main network and testnet3 definitions
+
+params = {
+    'bitcoin_main': {
+        'pubkey_address': 23,
+        'script_address': 13,
+        'genesis_hash': '4f6e11d6600f04da4eea1402879e8ce31bb3954d6998f6695f68dbea2c638ba8'
+    },
+    'bitcoin_test': {
+        'pubkey_address': 111,
+        'script_address': 196,
+        'genesis_hash': '000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943'
+    }
+}
